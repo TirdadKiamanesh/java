f9abee53b0a007b2feb83e5d93c182f809515ca2
Tirdad Kiamanesh <tkiamanesh@id.iit.edu>
2017-03-31 11:39:54 -0500

    create an event for box 0

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ var boxes= document.querySelectorAll(".box");
 boxes[0].style.backgroundColor = "blue" ;
 boxes[2].style.backgroundColor = "yellow" ;
 
+boxes[0].addEventListener( "click", boxClicked);
+
+function boxClicked () {
+    console.log( "Hey. box clicked!")
+}
+
 console.log(boxes[0]);
