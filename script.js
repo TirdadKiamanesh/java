fbee9b984ebe653784b63705afacc91167eb231b
Tirdad Kiamanesh <tkiamanesh@id.iit.edu>
2017-04-06 01:38:41 -0500

    more or less done, just the last top margin remains

--- script.js
+++ script.js
@@ -17,15 +17,15 @@ var leftMargin = 0;
 var leftMargin1 = 0;
 var leftMargin2 = 0;
 var leftMargin3 = 0;
-var leftMargin4 = 0;
-var leftMargin5 = 0;
+var topmMargin = 0;
+
 // Variable used to add to the left margin.
 var moveAmount = 5;
 var moveAmount1 = 10;
 var moveAmount2 = 15;
 var moveAmount3 = 20;
 var moveAmount4 = 25;
-var moveAmount4 = 25;
+
 
 
 
@@ -39,8 +39,45 @@ function boxClicked() {
     leftMargin += moveAmount;
     boxes[0].style.marginLeft = leftMargin + "px";
 }
-    boxes[2].style.marginLeft2 = leftMargin2 + "px";
+
+function box1Clicked() {
+
+    console.log("Hey, box1 clicked!");
+
+    leftMargin1 += moveAmount1;
+    boxes[1].style.marginLeft = leftMargin1 + "px";
+}
+
+function box2Clicked() {
+    
+    console.log("Hey, box2 clicked!");
+
+    leftMargin2 += moveAmount2;
+    boxes[2].style.marginLeft = leftMargin2 + "px";
+}
+
+
+function box3Clicked() {
+    
+    console.log("Hey, box3 clicked!");
+
+    leftMargin3 += moveAmount3;
+    boxes[3].style.marginLeft = leftMargin3 + "px";
+}
+
+function box4Clicked() {
+    
+    console.log("Hey, box4 clicked!");
+
+    topmMargin += moveAmount4;
+    boxes[4].style.topmMargin = topmMargin + "px";
+}
+
 
 // When anyone clicks on thefirst box, call the function "boxClicked".
 boxes[0].addEventListener("click", boxClicked);
-boxes[2].addEventListener("click", boxClicked);
+boxes[1].addEventListener("click", box1Clicked);
+boxes[2].addEventListener("click", box2Clicked);
+boxes[3].addEventListener("click", box3Clicked);
+boxes[4].addEventListener("click", box4Clicked);
+
